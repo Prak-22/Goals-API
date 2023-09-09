9fc4fbabd3f22053f77773f157cb030038082e29
Prakruti Priyadarshini <115170836+Prak-22@users.noreply.github.com>
2023-09-09 13:57:51 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+const connectDB = require("./backend/config/db")
+const {errorHandler} = require('./backend/middleware/errorMiddleware')
+
+connectDB()
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/goals', require('./backend/routes/goalRoutes'))
+app.use('/api/users', require('./backend/routes/userRoutes'));
+
+app.use(errorHandler)//express errorHandler
+
+app.listen(port, () => console.log(`On PORT ${port}`))
